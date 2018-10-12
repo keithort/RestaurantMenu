a910306bf14bde100545c611da1eba871b28d391
Keith Ort <keith@verticallatency.com>
2018-10-12 14:57:10 -0400

    Link to AmazeeLabs.com in header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,11 +8,17 @@ export default class Header extends Component {
     return (
       <header>
         <h1 className={css({ padding: "0 1em" })}>
-          <img
-            src={Logo}
-            className={css({ maxWidth: "5em", height: "auto", width: "100%" })}
-            alt="Amazee Labs Cafe"
-          />
+          <a href="https://amazeelabs.com">
+            <img
+              src={Logo}
+              className={css({
+                maxWidth: "5em",
+                height: "auto",
+                width: "100%"
+              })}
+              alt="Amazee Labs Cafe"
+            />
+          </a>
         </h1>
       </header>
     );
